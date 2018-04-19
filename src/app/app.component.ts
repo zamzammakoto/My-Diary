e32c063b8ebeb53396baf8a164543d4dcd73c1e9
zamzammakoto <makotozamzam7@gmail.com>
2018-04-19 15:12:11 +0300

    added header and a paparagraph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1> My Diary </h1>
+    <p> My diary for today is about {{diary}} </p>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  diary = 'My Thoughts ';
 }
